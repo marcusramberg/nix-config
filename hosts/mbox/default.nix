300d90fd47aed9e644a9bdd66ec300921be3022a
Marcus Ramberg <marcus@means.no>
2023-02-12 22:51:06 +0100

    Fix wsl

--- hosts/mbox/default.nix
+++ hosts/mbox/default.nix
@@ -4,18 +4,38 @@ let
   nixos-wsl = import ./nixos-wsl;
 in
 {
+  networking.hostName = "mbox";
   wsl = {
     enable = true;
     wslConf.automount.root = "/mnt";
+    # nativeSystemd = true;
     defaultUser = "marcus";
     startMenuLaunchers = true;
 
     # Enable native Docker support
-    docker-native.enable = true;
+    # docker-native.enable = true;
 
     # Enable integration with Docker Desktop (needs to be installed)
     # docker-desktop.enable = true;
 
+  };
+    systemd.services.nixs-wsl-systemd-fix = {
+    description = "Fix the /dev/shm symlink to be a mount";
+    unitConfig = {
+      DefaultDependencies = "no";
+      Before = [ "sysinit.target" "systemd-tmpfiles-setup-dev.service" "systemd-tmpfiles-setup.service" "systemd-sysctl.service" ];
+      ConditionPathExists = "/dev/shm";
+      ConditionPathIsSymbolicLink = "/dev/shm";
+      ConditionPathIsMountPoint = "/run/shm";
+    };
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = [
+        "${pkgs.coreutils-full}/bin/rm /dev/shm"
+        "/run/wrappers/bin/mount --bind -o X-mount.mkdir /run/shm /dev/shm"
+      ];
+    };
+    wantedBy = [ "sysinit.target" ];
   };
 
   # Enable nix flakes
@@ -24,5 +44,5 @@ in
   #   experimental-features = nix-command flakes
   # '';
 
-  system.stateVersion = "22.11";
+#  system.stateVersion = "22.11";
 }
