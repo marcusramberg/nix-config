c9ebee98a01e52d4520525a3ed84800187656646
Marcus Ramberg <marcus@means.no>
2023-02-15 09:02:00 +0100

    chore: Re-add karabiner config, add ctrl-r for neovim

--- home/files.nix
+++ home/files.nix
@@ -1,11 +1,12 @@
 { pkgs, ... }:
 {
   ".config/wezterm" = { source = ./files/config/wezterm; recursive = true; };
-  ".config/nvim" = { source = ./files/config/nvim; recursive = true; };
+  ".config/jvim" = { source = ./files/config/nvim; recursive = true; };
 
   ".config/fish/tide_config.fish".source = ./files/config/fish/tide_config.fish;
   ".config/bat/config".source = ./files/config/bat/config;
   ".config/gh/config.yml".source = ./files/config/gh/config.yml;
+  ".config/karabiner/karabiner.json".source = ./files/config/karabiner.json;
   ".i3/config".source = ./files/i3-config;
   ".ripgreprc".source = ./files/ripgreprc;
   ".gitconfig".source = ./files/gitconfig;
--- home/files/config/nvim/lua/config/keymaps.lua
+++ home/files/config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,7 @@
 -- -- Add any additional keymaps here
 --
 vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>", { desc = "Find Files" })
+vim.keymap.set("n", "<C-r>", "<cmd>Telescope oldfiles<cr>", { desc = "Find Files" })
 vim.keymap.set("n", "<leader>pp", "<cmd>Telescope project<cr>", { desc = "Switch Project" })
 vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Neogit" })
 vim.keymap.set("n", "<leader>gG", "<cmd>Neogit cwd=.<cr>", { desc = "Neogit (cwd)" })
