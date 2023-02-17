57a84b24366d0eac654bd4efe8e4d5aa6e487b1f
Marcus Ramberg <marcus@means.no>
2023-02-17 18:17:03 +0100

    refactor: Move all non-nix config to /config

--- home/files.nix
+++ home/files.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 {
-  ".config/wezterm" = { source = ./files/config/wezterm; recursive = true; };
-  ".config/nvim" = { source = ./files/config/nvim; recursive = true; };
+  ".config/wezterm" = { source = ../config/wezterm; recursive = true; };
+  ".config/nvim" = { source = ../config/nvim; recursive = true; };
 
-  ".config/fish/tide_config.fish".source = ./files/config/fish/tide_config.fish;
-  ".config/bat/config".source = ./files/config/bat/config;
-  ".config/gh/config.yml".source = ./files/config/gh/config.yml;
-  ".config/karabiner/karabiner.json".source = ./files/config/karabiner.json;
-  ".i3/config".source = ./files/i3-config;
-  ".ripgreprc".source = ./files/ripgreprc;
-  ".gitconfig".source = ./files/gitconfig;
+  ".config/fish/tide_config.fish".source = ../config/fish/tide_config.fish;
+  ".config/bat/config".source = ../config/bat/config;
+  ".config/gh/config.yml".source = ../config/gh/config.yml;
+  ".config/karabiner/karabiner.json".source = ../config/karabiner.json;
+  ".i3/config".source = ../config/i3-config;
+  ".ripgreprc".source = ../config/ripgreprc;
+  ".gitconfig".source = ../config/gitconfig;
 }
 
