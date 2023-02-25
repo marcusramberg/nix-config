36a559fd4c11e1435a0cdf09877def60e58e31dd
Marcus Ramberg <marcus@means.no>
2023-02-25 12:40:28 +0100

    chore: Remove useless blame plugin

--- config/nvim/lua/plugins/vcs.lua
+++ config/nvim/lua/plugins/vcs.lua
@@ -15,7 +15,6 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
   },
   { "akinsho/git-conflict.nvim", config = true },
-  "f-person/git-blame.nvim",
   {
     "pwntester/octo.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim", "nvim-tree/nvim-web-devicons" },
