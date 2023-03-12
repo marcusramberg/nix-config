f1e0d4ad20c92613d3aee07b38833b9197fdc9a1
Marcus Ramberg <marcus@means.no>
2023-03-12 13:29:33 +0100

    fix: uhm, maybe test stuff before just pushing..

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "nix.means.no";
 
   inputs = {
-    nixpkgs.url = "github:nixpkgs/nixos-unstable";
-    stable.url = "github:nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    stable.url = "github:nixos/nixpkgs/nixos-22.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     neovim-nightly-overlay = {
