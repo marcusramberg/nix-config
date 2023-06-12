f3a90d0409fe20fc852e51bc6522407bf2dc2168
Marcus Ramberg <marcus@means.no>
2023-06-12 09:51:29 +0200

    fix: vivialdi handler. Remove discord as it's not on arm

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -3,7 +3,6 @@
 {
   environment.systemPackages = with pkgs; [
     copyq
-    discord
     element-desktop
     feh
     nitrogen
@@ -73,11 +72,11 @@
   };
   networking.firewall.allowedTCPPorts = [ 3389 ];
   xdg.mime.defaultApplications = {
-    "text/html" = "vivaldi.desktop";
-    "x-scheme-handler/http" = "vivaldi.desktop";
-    "x-scheme-handler/https" = "vivaldi.desktop";
-    "x-scheme-handler/about" = "vivaldi.desktop";
-    "x-scheme-handler/unknown" = "vivaldi.desktop";
+    "text/html" = "vivaldi-stable.desktop";
+    "x-scheme-handler/http" = "vivaldi-stable.desktop";
+    "x-scheme-handler/https" = "vivaldi-stable.desktop";
+    "x-scheme-handler/about" = "vivaldi-stable.desktop";
+    "x-scheme-handler/unknown" = "vivaldi-stable.desktop";
   };
   environment.sessionVariables.DEFAULT_BROWSER = "${pkgs.vivaldi}/bin/vivaldi";
 
