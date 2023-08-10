fbda0b735e2c37a2b932ba1e16e6a0b4572eccaf
Marcus Ramberg <marcus@means.no>
2023-08-11 00:15:39 +0200

    feat(wip): moctopi

--- hosts/moctopi/default.nix
+++ hosts/moctopi/default.nix
@@ -1,1 +1,26 @@
-_: { }
+{ ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../modules/desktop.nix
+    ../../modules/pipewire.nix
+  ];
+  networking.hostName = "butterbee"; # Define your hostname.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  hardware.enableAllFirmware = true;
+  hardware.pulseaudio.enable = false;
+  services.pipewire.audio.enable = true;
+
+  services.flatpak.enable = true;
+  virtualisation.podman.enable = true;
+  virtualisation.podman.dockerCompat = true;
+  networking.extraHosts = ''
+    10.211.55.2 mbook
+    0.0.0.0 vg.no www.vg.no
+  '';
+}
+>>>>>>> Stashed changes
---
+++ lib/mkPiImage.nix
@@ -0,0 +1,18 @@
+name:
+{ nixpkgs, inputs, system, user, overlays }:
+
+let mkOptions = import ./mkOptions.nix;
+in nixpkgs.lib.nixosSystem {
+  specialArgs = { inherit inputs; };
+  inherit system;
+  modules = [
+    ../hosts/${name}
+  "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+    inputs.home-manager.nixosModules.home-manager
+    (mkOptions system {
+      inherit user;
+      inherit overlays;
+      inherit inputs;
+    })
+  ];
+}
