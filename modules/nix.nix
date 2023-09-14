11a0923c3bc83818b301b09616d86d01465af04a
Marcus Ramberg <marcus@means.no>
2023-09-14 19:54:31 +0200

    feat: Add nix helm cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -5,11 +5,13 @@
         "https://cache.nixos.org/"
         "https://cache.floxdev.com"
         "https://nix-community.cachix.org"
+        "https://nixhelm.cachix.org"
       ];
       trusted-public-keys = [
         "flox-store-public-0:8c/B+kjIaQ+BloCmNkRUKwaVPFWkriSAd0JJvuDu4F0="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nixhelm.cachix.org-1:esqauAsR4opRF0UsGrA6H3gD21OrzMnBBYvJXeddjtY="
       ];
       experimental-features = [ "nix-command" "flakes" ];
       trusted-users = [ "marcus" ];
