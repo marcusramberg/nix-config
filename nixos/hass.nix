acc19d3c8add456ab39e252bced972a4419a5f9e
Marcus Ramberg <marcus@means.no>
2023-10-17 22:30:33 +0200

    chore: Up hass

--- nixos/hass.nix
+++ nixos/hass.nix
@@ -32,7 +32,7 @@ in {
     virtualisation.oci-containers.containers = {
       hass = {
         # renovate: datasource=docker depName=homeassistant/home-assistant
-        image = "ghcr.io/home-assistant/home-assistant:2023.9.2";
+        image = "ghcr.io/home-assistant/home-assistant:2023.10.3";
         environment = { TZ = "Europe/Oslo"; };
         extraOptions = [
           "--net=host"
@@ -43,7 +43,7 @@ in {
       };
       zwave-js-ui = {
         # renovate: datasource=docker depName=zwave-js/zwave-js-ui
-        image = "ghcr.io/zwave-js/zwave-js-ui:8.26.0";
+        image = "ghcr.io/zwave-js/zwave-js-ui:9.1.2";
         volumes = [
           "/var/lib/zwave-js-ui:/usr/src/app/store"
           "/dev:/dev"
