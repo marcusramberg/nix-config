f73987c9f0449f688efe8a1aa50a3dbc38ce603e
Marcus Ramberg <marcus@means.no>
2024-01-12 18:44:33 +0100

    feat: initial work on gitea module

---
+++ modules/gitea.nix
@@ -0,0 +1,21 @@
+{ lib, config, ... }:
+with lib;
+let cfg = config.profiles.gitea;
+in {
+
+  options.profiles.gitea = {
+    enable = mkEnableOption "Gitea server";
+    host = mkOption {
+      type = types.str;
+      description = "Domain name for the Gitea server";
+      default = "git.means.no";
+    };
+
+  };
+
+  config = mkIf cfg.server.enable { };
+  services.gitea = {
+    enable = true;
+    settings = { service = { ROOT_URL = cfg.host; }; };
+  };
+}
