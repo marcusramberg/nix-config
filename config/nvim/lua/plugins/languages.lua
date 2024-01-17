26c6d0f1e43dc10848e75b479778505a0d18ec1c
Marcus Ramberg <marcus@means.no>
2024-01-17 19:53:04 +0100

    feat: QML support

--- config/nvim/lua/plugins/languages.lua
+++ config/nvim/lua/plugins/languages.lua
@@ -24,65 +24,6 @@ return {
 			return opts
 		end,
 	},
-	-- {
-	--   "neovim/nvim-lspconfig",
-	--   ---@class PluginLspOpts
-	--   opts = {
-	--     servers = {
-	--       nim_langserver = {},
-	--       jsonls = {
-	--         settings = {
-	--           schemas = {
-	--             description = "TypeScript compiler configuration file",
-	--             fileMatch = { "tsconfig.json", "tsconfig.*.json" },
-	--             url = "http://json.schemastore.org/tsconfig",
-	--           },
-	--           {
-	--             description = "Lerna config",
-	--             fileMatch = { "lerna.json" },
-	--             url = "http://json.schemastore.org/lerna",
-	--           },
-	--           {
-	--             description = "Babel configuration",
-	--             fileMatch = { ".babelrc.json", ".babelrc", "babel.config.json" },
-	--             url = "http://json.schemastore.org/lerna",
-	--           },
-	--           {
-	--             description = "ESLint config",
-	--             fileMatch = { ".eslintrc.json", ".eslintrc" },
-	--             url = "http://json.schemastore.org/eslintrc",
-	--           },
-	--           {
-	--             description = "Bucklescript config",
-	--             fileMatch = { "bsconfig.json" },
-	--             url = "https://bucklescript.github.io/bucklescript/docson/build-schema.json",
-	--           },
-	--           {
-	--             description = "Prettier config",
-	--             fileMatch = { ".prettierrc", ".prettierrc.json", "prettier.config.json" },
-	--             url = "http://json.schemastore.org/prettierrc",
-	--           },
-	--           {
-	--             description = "Vercel Now config",
-	--             fileMatch = { "now.json" },
-	--             url = "http://json.schemastore.org/now",
-	--           },
-	--           {
-	--             description = "Stylelint config",
-	--             fileMatch = { ".stylelintrc", ".stylelintrc.json", "stylelint.config.json" },
-	--             url = "http://json.schemastore.org/stylelintrc",
-	--           },
-	--           {
-	--             description = "Manifest v3",
-	--             fileMatch = { "manifest.json" },
-	--             url = "https://json.schemastore.org/chrome-manifest",
-	--           },
-	--         },
-	--       },
-	--       tflint = {},
-	--     },
-	--   },
-	-- },
 	{
 		"williamboman/mason-lspconfig.nvim",
 		enabled = false,
@@ -108,8 +49,8 @@ return {
 				"markdown",
 				"markdown_inline",
 				"nix",
-				-- Commented out as we have to do it by hand for now
-				-- "nim",
+				"nim",
+				"qmljs",
 				"query",
 				"regex",
 				"svelte",
