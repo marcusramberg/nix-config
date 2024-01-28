f6c2cbe52c60c58f304217a668b89d36f4a1512d
Marcus Ramberg <marcus@means.no>
2024-01-28 13:02:27 +0100

    feat: Configuration for neovide

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -9,3 +9,25 @@ opt.synmaxcol = 300
 opt.iskeyword:append("-")
 opt.foldenable = false
 opt.relativenumber = false
+
+if vim.g.neovide then
+	vim.g.neovide_confirm_quit = true
+	-- Clipboard
+	vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+	-- Scaling
+	vim.g.neovide_scale_factor = 1.0
+	local change_scale_factor = function(delta)
+		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+	end
+	vim.keymap.set("n", "<C-=>", function()
+		change_scale_factor(1.25)
+	end)
+	vim.keymap.set("n", "<C-->", function()
+		change_scale_factor(1 / 1.25)
+	end)
+end
