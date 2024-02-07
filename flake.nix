aa47e8b73f6a96c16a14b2b20bf2bd3956385c88
Marcus Ramberg <marcus@means.no>
2024-02-07 22:17:02 +0100

    feat: Switch to using nixos-small

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
     nix-index-database.url = "github:Mic92/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
     nix-std.url = "github:chessai/nix-std";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     nur.url = "github:nix-community/NUR";
     stable.url = "github:nixos/nixpkgs/nixos-23.11";
     tfenv.flake = false;
