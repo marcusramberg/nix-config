cbaa8995de180c3ddc27e2dd8a6c4e8c366dfe36
Marcus Ramberg <marcus@means.no>
2024-04-20 18:24:50 +0200

    refactor: Move escapetime into nix options

--- home/tmux.nix
+++ home/tmux.nix
@@ -4,12 +4,15 @@
     baseIndex = 1;
     clock24 = true;
     enable = true;
+    # Instant esc
+    escapeTime = 0;
     keyMode = "vi";
     plugins = with pkgs.tmuxPlugins; [ nord tmux-thumbs tmux-fzf tilish ];
     prefix = "`";
     secureSocket = false; # survives user logout
     terminal = "tmux-256color";
     tmuxinator.enable = true;
+
     extraConfig = "${pkgs.lib.readFile ../config/tmux.conf}";
   };
 }
