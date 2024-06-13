2a43ffdf88122d77d688c6d300b4e5ee0803982b
Marcus Ramberg <marcus@means.no>
2024-06-13 21:51:13 +0200

    feat: Add bash setup for when you can't chsh to fish

--- home/fish.nix
+++ home/fish.nix
@@ -8,6 +8,16 @@ let
   inherit (pkgs.stdenv) isDarwin;
 in
 {
+  programs.bash = {
+    enable = true;
+    initExtra = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
   programs.fish = {
     enable = true;
     functions = {
