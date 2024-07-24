af21be1714509a8302ec97ccb174eaabf3292b8c
Marcus Ramberg <marcus@means.no>
2024-07-24 21:21:05 +0200

    refactor: Use small, move cmp down where it's used

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,6 @@
 -- -- Add any additional keymaps here
 --
 local keymap = vim.keymap.set
-local cmp = require("cmp")
 keymap({ "n", "v", "i" }, "<C-p>", "<cmd>Telescope find_files<cr>", { desc = "Find Files" })
 keymap("n", "U", "<C-r>", { desc = "Redo" })
 keymap("n", "<C-r>", "<cmd>Telescope oldfiles<cr>", { desc = "Find Files" })
@@ -11,6 +10,7 @@ keymap("n", "<leader>pp", "<cmd>Telescope project<cr>", { desc = "Switch Project
 keymap("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Neogit" })
 keymap("n", "<leader>gG", "<cmd>Neogit cwd=.<cr>", { desc = "Neogit (cwd)" })
 keymap("n", "<leader>gb", "<cmd>BlamerToggle<cr>", { desc = "Toggle Blame" })
+local cmp = require("cmp")
 keymap("i", "<C-CR>", function()
 	cmp.complete()
 end, { desc = "Complete" })
