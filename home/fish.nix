5fa5990c1cd5697c7516adcd42103d9ee59b9ddc
Marcus Ramberg <marcus@means.no>
2024-08-09 14:36:24 +0200

    fix: Disable bash exec of fish as it breaks nix-shell

--- home/fish.nix
+++ home/fish.nix
@@ -10,13 +10,13 @@ in
 {
   programs.bash = {
     enable = true;
-    initExtra = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
-    '';
+    # initExtra = ''
+    #   if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+    #   then
+    #     shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+    #     exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+    #   fi
+    # '';
   };
   programs.fish = {
     enable = true;
@@ -84,7 +84,6 @@ in
       type -q kustomize; and eval (kustomize completion fish)
       type -q yq; and yq shell-completion fish | source
 
-      # any-nix-shell fish --info-right | source
     '';
     loginShellInit = ''
       gpgconf --launch gpg-agent
