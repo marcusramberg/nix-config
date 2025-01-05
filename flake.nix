80efc0347cfa169b893d17f5c12695f3ed068a00
Marcus Ramberg <marcus@means.no>
2025-01-06 00:01:05 +0100

    chore: revert to unstable

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
     nix-index-database.url = "github:Mic92/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
     nix-std.url = "github:chessai/nix-std";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # Do we actually use nur?
     nur.url = "github:nix-community/NUR";
     tfenv.flake = false;
