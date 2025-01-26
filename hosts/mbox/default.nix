f53a3853d87ed23d740eddbd4c3ac74881f27c62
Marcus Ramberg <marcus@means.no>
2025-01-26 12:20:00 +0100

    fix: caddy hash

--- hosts/mbox/default.nix
+++ hosts/mbox/default.nix
@@ -144,7 +144,8 @@
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
-        hash = "sha256-WGV/Ve7hbVry5ugSmTYWDihoC9i+D3Ct15UKgdpYc9U=";
+        hash = "sha256-jCcSzenewQiW897GFHF9WAcVkGaS/oUu63crJu7AyyQ=";
+
       };
       configFile = ../../config/Caddyfile.mbox;
       adapter = "caddyfile";
