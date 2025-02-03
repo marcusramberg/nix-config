fbf6acbf3e85629080e64b4d606b1b4ad6837f66
Marcus Ramberg <marcus@means.no>
2025-02-03 18:00:48 +0100

    chore: bump, add some missing files

---
+++ cachix/static-haskell-nix.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://static-haskell-nix.cachix.org"
+      ];
+      trusted-public-keys = [
+        "static-haskell-nix.cachix.org-1:Q17HawmAwaM1/BfIxaEDKAxwTOyRVhPG5Ji9K3+FvUU="
+      ];
+    };
+  };
+}
