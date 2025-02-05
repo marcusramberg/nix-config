380c5fb8b77f455f42e1f23182bd9c3525244465
Marcus Ramberg <marcus@means.no>
2025-02-05 10:22:03 +0100

    chore: devenv needs impurity for bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nix develop --extra-experimental-features nix-command --extra-experimental-features flakes
+nix develop --impure . --extra-experimental-features nix-command --extra-experimental-features flakes
