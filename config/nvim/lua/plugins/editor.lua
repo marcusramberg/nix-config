778e096c58046f35f37c024616d5fd7337298361
Marcus Ramberg <marcus@means.no>
2025-02-08 15:30:33 +0100

    feat: implement autosave

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -18,7 +18,13 @@ return {
   -- Simplify the macro syntax
   { "chrisgrieser/nvim-recorder", config = true },
   { "metakirby5/codi.vim" },
-  { "Pocco81/auto-save.nvim" },
+  {
+    "okuuva/auto-save.nvim",
+    version = "*", -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {},
+  },
   {
     "ibhagwan/smartyank.nvim",
     config = function()
