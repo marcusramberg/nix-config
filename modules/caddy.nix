c7545b07be255d2da9350a6de1e4a9af7ffa7630
Marcus Ramberg <marcus@means.no>
2025-03-02 22:01:12 +0100

    chore: update mhub

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -24,7 +24,7 @@ in
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
-        hash = "sha256-JVkUkDKdat4aALJHQCq1zorJivVCdyBT+7UhqTvaFLw=";
+        hash = "sha256-kbTKCPjjIGRZZ550lBg0c5Ye4AK4o5yCRynBIvCLYkQ=";
       };
       inherit (cfg) configFile;
       adapter = "caddyfile";
