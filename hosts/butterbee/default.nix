5acf56ab633ee09dad44d70b6ba9675d2addef65
Marcus Ramberg <marcus@means.no>
2025-03-10 10:12:47 +0100

    fix: rely on the module

--- hosts/butterbee/default.nix
+++ hosts/butterbee/default.nix
@@ -37,9 +37,6 @@
   systemd.packages = [ pkgs.cloudflare-warp ];
   systemd.user.services.warp-taskbar.wantedBy = [ "graphical.target" ];
 
-  virtualisation.podman.enable = true;
-  virtualisation.podman.dockerCompat = true;
-
   networking.extraHosts = ''
     10.211.55.2 mbook
     0.0.0.0 vg.no www.vg.no
