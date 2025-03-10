2294559a666f137bd8b09b90b3107d9c295fc123
Marcus Ramberg <marcus@means.no>
2025-03-10 10:13:16 +0100

    feat: some keybindings for octo

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -8,8 +8,24 @@ keymap("n", "U", "<C-r>", { desc = "Redo" })
 keymap("n", "<C-r>", "<cmd>Telescope oldfiles<cr>", { desc = "Find Files" })
 keymap("n", "<leader>pp", "<cmd>Telescope project<cr>", { desc = "Switch Project" })
 keymap("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Neogit" })
+keymap("n", "<leader>go", "<cmd>Octo<cr>", { desc = "Octo" })
 keymap("n", "<leader>gG", "<cmd>Neogit cwd=.<cr>", { desc = "Neogit (cwd)" })
 keymap("n", "<leader>gb", "<cmd>BlamerToggle<cr>", { desc = "Toggle Blame" })
 vim.keymap.del("n", "<A-j>")
 vim.keymap.del("n", "<A-k>")
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+
+-- Octo nvim ideas
+-- https://docs.github.com/en/search-github/searching-on-github/searching-issues-and-pull-requests
+keymap(
+  "n",
+  "<Leader>Gi",
+  "<cmd>Octo search is:pr involves:@me state:open<cr>",
+  { desc = "Octo » PRs involving me", silent = true }
+)
+keymap(
+  "n",
+  "<Leader>Gr",
+  "<cmd>Octo search is:pr review:required state:open review-requested:@me<cr>",
+  { desc = "Octo » PRs involving me", silent = true }
+)
