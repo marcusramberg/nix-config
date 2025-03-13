fe5371dc87746b7d379a6afc9b4922453f44af55
Marcus Ramberg <marcus@means.no>
2025-03-13 14:13:28 +0100

    chore: update name of deprecated option

--- darwin/default.nix
+++ darwin/default.nix
@@ -51,7 +51,7 @@
     nixPath = [ "nixpkgs=/run/current-system/sw/nixpkgs" ];
   };
   # Add ability to used TouchID for sudo authentication
-  security.pam.enableSudoTouchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
   time.timeZone = "Europe/Oslo";
 
   system.stateVersion = 4;
