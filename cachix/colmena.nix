3ec3750cbf5012a4d228840fa8d0515e4f54d015
Marcus Ramberg <marcus@means.no>
2025-03-17 23:28:20 +0100

    chore: add colmena cachix

---
+++ cachix/colmena.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://colmena.cachix.org"
+      ];
+      trusted-public-keys = [
+        "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
+      ];
+    };
+  };
+}
