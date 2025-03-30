59df686493313b63e75d2075f520b364a6ec6d68
Marcus Ramberg <marcus@means.no>
2025-03-30 18:23:17 +0200

    fix: tmux config

--- home/tmux.nix
+++ home/tmux.nix
@@ -9,15 +9,25 @@
     escapeTime = 0;
     keyMode = "vi";
     plugins = with pkgs.tmuxPlugins; [
-      catppuccin
-      tmux-thumbs
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavor "mocha"
+          set -g @catppuccin_window_status_style "rounded"
+        '';
+      }
+      {
+        plugin = tmux-thumbs;
+        extraConfig = ''
+          set -g @thumbs-osc52 1
+        '';
+      }
       tmux-fzf
     ];
     prefix = "`";
     secureSocket = false; # survives user logout
     terminal = "tmux-256color";
     tmuxinator.enable = true;
-
     extraConfig = "${pkgs.lib.readFile ../config/tmux.conf}";
   };
 }
