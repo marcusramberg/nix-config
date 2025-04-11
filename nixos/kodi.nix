1a2b02c40a03bb044d9819a2401fab41cff16317
Marcus Ramberg <marcus@means.no>
2025-04-11 12:28:39 +0200

    chore: fix deprecations

--- nixos/kodi.nix
+++ nixos/kodi.nix
@@ -14,38 +14,42 @@ in
   };
   config = mkIf cfg.enable {
     programs.dconf.enable = true;
-    services.avahi.enable = true;
-    services.xserver = {
-      enable = true;
-      desktopManager.kodi = {
-        enable = true;
-        package = pkgs.kodi.withPackages (
-          p: with p; [
-            invidious
-            kodi-platform
-            libretro-2048
-            mediacccde
-            netflix
-            pvr-iptvsimple
-            sendtokodi
-            somafm
-            vfs-sftp
-            visualization-matrix
-            visualization-projectm
-            visualization-shadertoy
-            visualization-spectrum
-            youtube
-          ]
-        );
-      };
+    services = {
+      avahi.enable = true;
       displayManager = {
-        lightdm = {
-          enable = true;
-          greeters.tiny.enable = true;
-        };
         defaultSession = "kodi";
         autoLogin.enable = true;
         autoLogin.user = "marcus";
+      };
+      xserver = {
+        enable = true;
+        desktopManager.kodi = {
+          enable = true;
+          package = pkgs.kodi.withPackages (
+            p: with p; [
+              invidious
+              kodi-platform
+              libretro-2048
+              mediacccde
+              netflix
+              pvr-iptvsimple
+              sendtokodi
+              somafm
+              vfs-sftp
+              visualization-matrix
+              visualization-projectm
+              visualization-shadertoy
+              visualization-spectrum
+              youtube
+            ]
+          );
+        };
+        displayManager = {
+          lightdm = {
+            enable = true;
+            greeters.tiny.enable = true;
+          };
+        };
       };
     };
   };
