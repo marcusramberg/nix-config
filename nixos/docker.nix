3d14fdb2b5d1a15dbb1c43796d3760360e6f8837
Marcus Ramberg <marcus@means.no>
2025-06-02 14:13:35 +0200

    fix: improve podman settings

--- nixos/docker.nix
+++ nixos/docker.nix
@@ -7,12 +7,20 @@ in
 
   options.profiles.dockerHost.enable = mkEnableOption "dockerHost";
 
-  config.virtualisation = mkIf cfg.enable {
-    podman = {
-      enable = true;
-      dockerSocket.enable = true;
-      dockerCompat = true;
+  config = mkIf cfg.enable {
+    environment.etc."containers/registries.conf".text = ''
+      [registries.search]
+      registries = ['docker.io']
+    '';
+
+    virtualisation = {
+      podman = {
+        enable = true;
+        dockerSocket.enable = true;
+        dockerCompat = true;
+        defaultNetwork.settings.dns_enabled = true;
+      };
+      oci-containers.backend = "podman";
     };
-    oci-containers.backend = "podman";
   };
 }
