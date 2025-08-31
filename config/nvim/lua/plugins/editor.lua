52b8dabaf7c52ce34b6b6fc756f0f9ec4dfa1e62
Marcus Ramberg <marcus@means.no>
2025-08-31 11:29:15 +0200

    feat: tab autocomplete, bump

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -100,4 +100,15 @@ return {
       },
     },
   },
+  -- lua/plugins/tab-autocomplete.lua
+  {
+    "saghen/blink.cmp",
+    opts = function(_, opts)
+      opts.keymap = {
+        preset = "super-tab",
+        ["<Tab>"] = { "select_and_accept" },
+        ["<S-Tab>"] = { "select_prev" },
+      }
+    end,
+  },
 }
