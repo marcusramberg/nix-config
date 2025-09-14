fe5ebaea77901b2484e7b02019e52211210b2e36
Marcus Ramberg <marcus@means.no>
2025-09-14 19:57:24 +0200

    chore: remove obsolete anon access

--- nixos/hass.nix
+++ nixos/hass.nix
@@ -14,11 +14,6 @@ in
       influxdb.enable = true;
       mosquitto = {
         enable = true;
-        # listeners = [{
-        #   acl = [ "pattern readwrite #" ];
-        #   omitPasswordAuth = true;
-        #   settings.allow_anonymous = true;
-        # }];
         listeners = [
           {
             omitPasswordAuth = false;
