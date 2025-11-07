fc8468526a47b4050b55d4357031fc50466c56ca
Marcus Ramberg <marcus@means.no>
2025-11-08 00:56:40 +0100

    feat: universal clipboard using cmd-c/v

--- nixos/keyboardmap.nix
+++ nixos/keyboardmap.nix
@@ -9,7 +9,7 @@ let
 in
 {
   options.hardware.keyboard.dual-caps = {
-    enable = lib.mkEnableOption "Enable dual esc/ctrl caps lock";
+    enable = lib.mkEnableOption "Enable keyboard remapping (caps2esc/ctrl and cmd-v/c -> insert";
     swapAlt.enable = lib.mkEnableOption "Also swap alt/meta";
     swapAlt.device = lib.mkOption {
       type = lib.types.str;
@@ -52,26 +52,73 @@ in
               HOLD: KEY_LEFTMETA
               TAP: KEY_LEFTMETA
         '';
+        clipboard = ''
+          { .keys = { KEY_LEFTMETA, KEY_V },
+           .down_press = { KEY_LEFTSHIFT, KEY_RESERVED, KEY_INSERT, KEY_RESERVED },
+           .up_press   = { KEY_RESERVED, KEY_INSERT, KEY_RESERVED, KEY_LEFTSHIFT },
+           DOWN_IFF_ALL_DOWN(2) },
+          { .keys = { KEY_LEFTMETA, KEY_C },
+           .down_press = { KEY_CNT, KEY_RESERVED, KEY_INSERT, KEY_RESERVED },
+           .up_press   = { KEY_RESERVED, KEY_INSERT, KEY_RESERVED, KEY_CNT},
+           DOWN_IFF_ALL_DOWN(2) }
+        '';
+        universalClipboard = pkgs.stdenv.mkDerivation {
+          name = "universal-clipboard";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "zsugabubus";
+            repo = "interception-k2k";
+            rev = "5746bf39a321610bb6019781034f82e4c6e21e97";
+            hash = "sha256-q2zlOvyW5jlasEIPVc+k6jh2wJZ7sUEpvXh/leH/hKw=";
+          };
+          patches = [ ./k2k-multi.patch ];
+
+          configurePhase = ''
+            mkdir -p ./in/uc
+            echo "${clipboard}" > ./in/uc/multi-rules.h.in
+          '';
+
+          makeFlags = [
+            "OUT_DIR=$(out)"
+            "INSTALL_DIR=$(out)/bin"
+            "CONFIG_DIR=./in"
+          ];
+
+          meta = {
+            description = "Map cmd+c/v to shift/ctrl-insert for universal clipboard sharing";
+            mainProgram = "uc";
+          };
+        };
+        uc = "${universalClipboard}/bin/uc";
+        dual = "${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys";
+        intercept = "${pkgs.interception-tools}/bin/intercept";
+        uinput = "${pkgs.interception-tools}/bin/uinput";
+
       in
       {
         enable = true;
-        plugins = [ pkgs.interception-tools-plugins.dual-function-keys ];
+        plugins = [
+          pkgs.interception-tools-plugins.dual-function-keys
+          universalClipboard
+        ];
         udevmonConfig =
           (
             if cfg.swapAlt.enable then
               ''
-                - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys -c ${swapAlt} | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
-                  DEVICE:
+                - JOB: "${intercept} -g $DEVNODE | ${dual} -c ${swapAlt} | ${uinput} -d $DEVNODE" 
+                  DEVICE: 
                     LINK: /dev/input/by-path/${cfg.swapAlt.device}
               ''
             else
               ""
           )
           + ''
-            - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys -c ${dualFunctionKeysConfig} | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
-              DEVICE:
+            - JOB: "${intercept} -g $DEVNODE | ${dual} -c ${dualFunctionKeysConfig} | ${uinput} -d $DEVNODE"
+              DEVICE: 
                 LINK: .*-event-kbd
-                
+            - JOB: "${intercept} -g $DEVNODE | ${uc} | ${uinput} -d $DEVNODE"
+              DEVICE: 
+                LINK: .*-event-kbd
           '';
       };
     # link bluetooth keyboards
