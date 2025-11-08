f5adecc8efcc629cd5f1398182c1027ec864f604
Marcus Ramberg <marcus@means.no>
2025-11-08 14:16:36 +0100

    refactor: clean up keyboardmap, move universal-clipboard to separate package

--- nixos/keyboardmap.nix
+++ nixos/keyboardmap.nix
@@ -9,7 +9,7 @@ let
 in
 {
   options.hardware.keyboard.dual-caps = {
-    enable = lib.mkEnableOption "Enable keyboard remapping (caps2esc/ctrl and cmd-v/c -> insert";
+    enable = lib.mkEnableOption "Enable keyboard remapping (caps2esc/ctrl and cmd-v/c -> ctrl/shift-insert";
     swapAlt.enable = lib.mkEnableOption "Also swap alt/meta";
     swapAlt.device = lib.mkOption {
       type = lib.types.str;
@@ -25,26 +25,22 @@ in
         # Map caps lock to:
         # - ESC when tapped
         # - LCTRL when held
-        dualFunctionKeysConfig = pkgs.writeText "dual-function-keys.yaml" ''
+        dualFunctionKeys = ''
           MAPPINGS:
-            # CAPS (tap) -> ESC, CAPS (hold) -> LCTRL
+          # CAPS (tap) -> ESC, CAPS (hold) -> LCTRL
             - KEY: KEY_CAPSLOCK
               TAP: KEY_ESC
               HOLD: KEY_LEFTCTRL
+          # tab as hyper when used in a chord.
             - KEY: KEY_TAB
               TAP: KEY_TAB
               HOLD: [KEY_LEFTCTRL,KEY_LEFTMETA,KEY_LEFTALT,KEY_LEFTSHIFT]
               HOLD_START: BEFORE_CONSUME
         '';
+        dualFunctionKeysConfig = pkgs.writeText "dual-function-keys.yaml" dualFunctionKeys;
         swapAlt = pkgs.writeText "swap-alt.yaml" ''
-          MAPPINGS:
-            - KEY: KEY_CAPSLOCK
-              TAP: KEY_ESC
-              HOLD: KEY_LEFTCTRL
-            - KEY: KEY_TAB
-              TAP: KEY_TAB
-              HOLD: [KEY_LEFTCTRL,KEY_LEFTMETA,KEY_LEFTALT,KEY_LEFTSHIFT]
-              HOLD_START: BEFORE_CONSUME
+          ${dualFunctionKeys}
+          # swap alt and meta
             - KEY: KEY_LEFTMETA
               HOLD: KEY_LEFTALT
               TAP: KEY_LEFTALT
@@ -52,43 +48,7 @@ in
               HOLD: KEY_LEFTMETA
               TAP: KEY_LEFTMETA
         '';
-        clipboard = ''
-          { .keys = { KEY_LEFTMETA, KEY_V },
-           .down_press = { KEY_LEFTSHIFT, KEY_RESERVED, KEY_INSERT, KEY_RESERVED },
-           .up_press   = { KEY_RESERVED, KEY_INSERT, KEY_RESERVED, KEY_LEFTSHIFT },
-           DOWN_IFF_ALL_DOWN(2) },
-          { .keys = { KEY_LEFTMETA, KEY_C },
-           .down_press = { KEY_CNT, KEY_RESERVED, KEY_INSERT, KEY_RESERVED },
-           .up_press   = { KEY_RESERVED, KEY_INSERT, KEY_RESERVED, KEY_CNT},
-           DOWN_IFF_ALL_DOWN(2) }
-        '';
-        universalClipboard = pkgs.stdenv.mkDerivation {
-          name = "universal-clipboard";
-
-          src = pkgs.fetchFromGitHub {
-            owner = "zsugabubus";
-            repo = "interception-k2k";
-            rev = "5746bf39a321610bb6019781034f82e4c6e21e97";
-            hash = "sha256-q2zlOvyW5jlasEIPVc+k6jh2wJZ7sUEpvXh/leH/hKw=";
-          };
-          patches = [ ./k2k-multi.patch ];
-
-          configurePhase = ''
-            mkdir -p ./in/uc
-            echo "${clipboard}" > ./in/uc/multi-rules.h.in
-          '';
-
-          makeFlags = [
-            "OUT_DIR=$(out)"
-            "INSTALL_DIR=$(out)/bin"
-            "CONFIG_DIR=./in"
-          ];
-
-          meta = {
-            description = "Map cmd+c/v to shift/ctrl-insert for universal clipboard sharing";
-            mainProgram = "uc";
-          };
-        };
+        universalClipboard = import ../packages/universal-clipboard { inherit pkgs; };
         uc = "${universalClipboard}/bin/uc";
         dual = "${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys";
         intercept = "${pkgs.interception-tools}/bin/intercept";
@@ -102,16 +62,11 @@ in
           universalClipboard
         ];
         udevmonConfig =
-          (
-            if cfg.swapAlt.enable then
-              ''
-                - JOB: "${intercept} -g $DEVNODE | ${dual} -c ${swapAlt} | ${uinput} -d $DEVNODE" 
-                  DEVICE: 
-                    LINK: /dev/input/by-path/${cfg.swapAlt.device}
-              ''
-            else
-              ""
-          )
+          (lib.optionalString cfg.swapAlt.enable ''
+            - JOB: "${intercept} -g $DEVNODE | ${dual} -c ${swapAlt} | ${uinput} -d $DEVNODE" 
+              DEVICE: 
+                LINK: /dev/input/by-path/${cfg.swapAlt.device}
+          '')
           + ''
             - JOB: "${intercept} -g $DEVNODE | ${dual} -c ${dualFunctionKeysConfig} | ${uinput} -d $DEVNODE"
               DEVICE: 
---
+++ packages/universal-clipboard/default.nix
@@ -0,0 +1,40 @@
+{ pkgs, ... }:
+let
+  clipboard = ''
+    { .keys = { KEY_LEFTMETA, KEY_V },
+     .down_press = { KEY_LEFTSHIFT, KEY_RESERVED, KEY_INSERT, KEY_RESERVED },
+     .up_press   = { KEY_RESERVED, KEY_INSERT, KEY_RESERVED, KEY_LEFTSHIFT },
+     DOWN_IFF_ALL_DOWN(2) },
+    { .keys = { KEY_LEFTMETA, KEY_C },
+     .down_press = { KEY_CNT, KEY_RESERVED, KEY_INSERT, KEY_RESERVED },
+     .up_press   = { KEY_RESERVED, KEY_INSERT, KEY_RESERVED, KEY_CNT},
+     DOWN_IFF_ALL_DOWN(2) }
+  '';
+in
+pkgs.stdenv.mkDerivation {
+  name = "universal-clipboard";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "zsugabubus";
+    repo = "interception-k2k";
+    rev = "5746bf39a321610bb6019781034f82e4c6e21e97";
+    hash = "sha256-q2zlOvyW5jlasEIPVc+k6jh2wJZ7sUEpvXh/leH/hKw=";
+  };
+  patches = [ ./k2k-multi.patch ];
+
+  configurePhase = ''
+    mkdir -p ./in/uc
+    echo "${clipboard}" > ./in/uc/multi-rules.h.in
+  '';
+
+  makeFlags = [
+    "OUT_DIR=$(out)"
+    "INSTALL_DIR=$(out)/bin"
+    "CONFIG_DIR=./in"
+  ];
+
+  meta = {
+    description = "Map cmd+c/v to shift/ctrl-insert for universal clipboard sharing";
+    mainProgram = "uc";
+  };
+}
