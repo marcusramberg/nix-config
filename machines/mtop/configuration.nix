64065bd2170c6387eb67e6273b2f12409e95c576
Marcus Ramberg <marcus@means.no>
2025-12-01 23:11:27 +0100

    fix(mtop): switch to amdgpu, fix silly brightness issue

--- machines/mtop/configuration.nix
+++ machines/mtop/configuration.nix
@@ -3,7 +3,6 @@
 {
   imports = [
     # Include the results of the hardware scan.
-    ./hardware-configuration.nix
     ../../modules/pipewire.nix
   ];
   boot = {
@@ -14,13 +13,39 @@
       options snd_hda_intel model=mbp101
     '';
     kernelPackages = pkgs.linuxPackages_cachyos;
+    kernelParams = [
+      "acpi_backlight=native"
+    ];
   };
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/86952286-ffab-4b03-8a78-0c21099588bd";
+      fsType = "ext4";
+    };
+    "/home/marcus" = {
+      device = "/dev/disk/by-uuid/4d79d071-7bee-4cf0-85fa-01f313cb9eab";
+      fsType = "ext4";
+    };
+
+    "/boot/efi" = {
+      device = "/dev/disk/by-uuid/67E3-17ED";
+      fsType = "vfat";
+    };
+  };
+
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 8196;
+    }
+  ];
   services.scx.enable = true; # by default uses scx_rustland scheduler
 
   environment.systemPackages = with pkgs; [
     firedragon-catppuccin-bin
   ];
   hardware = {
+    amdgpu.legacySupport.enable = true;
     enableRedistributableFirmware = true;
     bluetooth.enable = false;
     keyboard.dual-caps.enable = true;
@@ -57,5 +82,18 @@
     AllowHybridSleep=no
     AllowSuspendThenHibernate=no
   '';
+  systemd.services.brightness-init = {
+    description = "brightness init";
+    wantedBy = [ "graphical.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      User = "root";
+      Group = "root";
+    };
+    script = ''
+      echo 150 > /sys/class/backlight/amdgpu_bl1/brightness
+    '';
+  };
 
 }
