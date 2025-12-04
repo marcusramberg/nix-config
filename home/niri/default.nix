f13fc912bf5ab872572e65cfd5d8d2679539f766
Marcus Ramberg <marcus@means.no>
2025-12-04 23:24:45 +0100

    feat: xwayland-satellite support

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -1,7 +1,7 @@
 {
-  config,
   lib,
   osConfig,
+  inputs,
   pkgs,
   ...
 }:
@@ -13,6 +13,9 @@ in
   home = mkIf isNiri {
     packages = with pkgs; [
       satty
+      wvkbd
+      inputs.nixpkgs-small.legacyPackages.${pkgs.stdenv.targetPlatform.system}.xwayland-satellite
+      wvkbd
     ];
     sessionVariables.QML2_IMPORT_PATH = lib.concatStringsSep ":" [
       "${pkgs.quickshell}/lib/qt-6/qml"
@@ -20,120 +23,5 @@ in
       "${pkgs.kdePackages.kirigami.unwrapped}/lib/qt-6/qml"
       "${pkgs.kdePackages.qtmultimedia}/lib/qt-6/qml"
     ];
-  };
-  programs = mkIf isNiri {
-    niri = {
-      # settings = {
-      #   hotkey-overlay = {
-      #     skip-at-startup = true;
-      #     hide-not-bound = true;
-      #   };
-      #   gestures.hot-corners.enable = false;
-      #   prefer-no-csd = true;
-      #   input = {
-      #     keyboard.xkb = {
-      #       inherit (osConfig.services.xserver.xkb) layout options variant;
-      #     };
-      #
-      #     touchpad = {
-      #       tap = true;
-      #       natural-scroll = true;
-      #     };
-      #
-      #     mouse = {
-      #       accel-profile = "flat";
-      #       natural-scroll = true;
-      #     };
-      #     workspace-auto-back-and-forth = true;
-      #   };
-      #   binds = import ./binds.nix { inherit config; };
-      #   outputs = {
-      #     "eDP-1".scale = 1.1;
-      #     "DP-1".scale = 1.4;
-      #   };
-      #
-      #   layout = {
-      #     gaps = 6;
-      #
-      #     center-focused-column = "never";
-      #     always-center-single-column = true;
-      #
-      #     preset-column-widths = [
-      #       { proportion = 0.3; }
-      #       { proportion = 0.5; }
-      #       { proportion = 0.7; }
-      #     ];
-      #
-      #     default-column-width = {
-      #       proportion = 0.5;
-      #     };
-      #
-      #     focus-ring = {
-      #       width = 1;
-      #       active = {
-      #         color = "#fab387";
-      #       };
-      #       inactive = {
-      #         color = "#b4befe";
-      #       };
-      #     };
-      #
-      #     shadow = {
-      #       enable = true;
-      #       softness = 30;
-      #       spread = 4;
-      #     };
-      #   };
-      #   window-rules = [
-      #     {
-      #       draw-border-with-background = false;
-      #       geometry-corner-radius = {
-      #         bottom-left = 0.0;
-      #         bottom-right = 0.0;
-      #         top-left = 0.0;
-      #         top-right = 0.0;
-      #       };
-      #     }
-      #     {
-      #       matches = [
-      #         {
-      #           app-id = "firefox$";
-      #           title = "^Picture-in-Picture$";
-      #         }
-      #       ];
-      #       open-floating = true;
-      #     }
-      #   ];
-      #   animations.workspace-switch.enable = false;
-      #   workspaces = {
-      #     "01-terminal" = {
-      #       name = "terminal";
-      #     };
-      #     "02-browser" = {
-      #       name = "browser";
-      #     };
-      #     "03-music" = {
-      #       name = "music";
-      #     };
-      #     "04-chat" = {
-      #       name = "chat";
-      #     };
-      #   };
-      #   environment = {
-      #     QT_QPA_PLATFORM = "wayland";
-      #     DMS_SCREENSHOT_EDITOR = "satty";
-      #   };
-      #   spawn-at-startup = [
-      #     {
-      #       argv = [
-      #         "${pkgs.wvkbd}/bin/wvkbd-mobintl"
-      #         "--alpha"
-      #         "220"
-      #         "--hidden"
-      #       ];
-      #     }
-      #   ];
-      # };
-    };
   };
 }
