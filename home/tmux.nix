4810d0470d29bd542dcce08413bced36bc6cd295
Marcus Ramberg <marcus@means.no>
2025-12-28 23:47:55 +0100

    chore: nord theme

--- home/tmux.nix
+++ home/tmux.nix
@@ -10,13 +10,7 @@
     keyMode = "vi";
     plugins = with pkgs.tmuxPlugins; [
       {
-        plugin = catppuccin;
-        extraConfig = ''
-          set -g @catppuccin_flavor "mocha"
-          set -g @catppuccin_window_status_style "rounded"
-          set -g status-right "#{E:@catppuccin_status_application}"
-          set -ag status-right "#{E:@catppuccin_status_session}"
-        '';
+        plugin = nord;
       }
       {
         plugin = tmux-thumbs;
