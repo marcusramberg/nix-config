c7418f146e4e04e78ea48fe8950aefedf92e724e
Marcus Ramberg <marcus@means.no>
2026-06-29 22:37:55 +0200

    mbox: updates from reinstall

--- machines/mdeck/configuration.nix
+++ machines/mdeck/configuration.nix
@@ -39,32 +39,33 @@
   ];
 
   # Automount SD card.
-  fileSystems."/run/media/deck/mmcblk0p1" = {
-    device = "/dev/mmcblk0p1";
-    options = [
-      "defaults"
-      "rw"
-      "nofail"
-      "x-systemd.automount"
-      "x-systemd.device-timeout=1ms"
-      "comment=x-gvfs-show"
-    ];
-  };
+  # fileSystems."/run/media/deck/mmcblk0p1" = {
+  #   device = "/dev/mmcblk0p1";
+  #   options = [
+  #     "defaults"
+  #     "rw"
+  #     "nofail"
+  #     "x-systemd.automount"
+  #     "x-systemd.device-timeout=1ms"
+  #     "comment=x-gvfs-show"
+  #   ];
+  # };
 
   hardware.keyboard.dual-caps = {
     enable = true;
     swapAlt.enable = true;
   };
 
-  profiles.desktop.enable = true;
+  profiles.desktop = {
+    enable = true;
+    displayManager = false;
+  };
 
   powerManagement.cpuFreqGovernor = "schedutil";
 
   programs.steam = {
     enable = true;
   };
-
-  environment.gnome.excludePackages = with pkgs; [ gnome-tour ];
 
   # Symlink old Steam Deck SD card path to new one.
   systemd.tmpfiles.rules = [ "L+ /run/media/mmcblk0p1 - - - - /run/media/deck/mmcblk0p1" ];
@@ -73,8 +74,6 @@
   networking.networkmanager.enable = true;
 
   services = {
-    displayManager.sddm.enable = lib.mkForce false;
-    displayManager.dms-greeter.enable = lib.mkForce false;
     input-remapper.enable = true;
     xserver = {
       enable = lib.mkForce true;
